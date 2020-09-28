cb150bee372e766fe5841105e539715e4c0d8d4f
frostyausty <priceaustin62@gmail.com>
2020-09-27 23:07:55 -0500

    added timer function

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,16 @@
 var startButton = document.getElementById('startBtn');
 var quizBox = document.getElementById('quiz');
 var startSection = document.getElementById('start');
+var endSection = document.getElementById('end');
 var questionEl = document.getElementById('question');
-var questionNumber = 0;
 var userAnswerEl = document.querySelector(".answers");
-var highScoreEl = 0;
 var highScoreDisplay = document.getElementById('highscore');
+var questionNumber = 0;
+var highScoreEl = 0;
+var timerMax = 70;
+var timePass=0;
 
-
-
+//quiz questions
 var questions = [
     {
         q: 'Commonly used data types DO NOT include:', 
@@ -37,12 +39,25 @@ var questions = [
     }
 ]
 
+//timer function takes total time subtracts the time that has passed, if it reaches 0 goes to endQuiz function
+var timer = function() {
+    timerTotal= timerMax - timePass;
+    timePass++;
+    document.getElementById('time').textContent=timerTotal;
+    if (timerTotal <= 0){
+        endQuiz();
+    }
+}
+
+//start quiz function 
 function startQuiz() {
     startSection.classList.add('hide');
     quizBox.classList.remove('hide');
+    StartTime = setInterval(timer,1000);
     nextQuestion()
 }
 
+//function that calls the next question, places answer options in the buttons
 function nextQuestion() {
     document.getElementById('question').textContent = questions[questionNumber].q;
     document.getElementById('btn1').textContent = questions[questionNumber].b[0];
@@ -50,31 +65,43 @@ function nextQuestion() {
     document.getElementById('btn3').textContent = questions[questionNumber].b[2];
     document.getElementById('btn4').textContent = questions[questionNumber].b[3];
 
+    //stores the value that user clicks
     userAnswerEl.addEventListener('click', userAnswer);   
-   // console.log(userAnswerEl);
-
-    //userAnswer();
 }
-
+//checks the answer to see if its correct
 function userAnswer(event) {
-    //console.log('hello')
     var userChoice = event.target;
-   
+   // if the answer is equal to the 'a' in the corresponding question it is correct, adds 15 to the score otherwise subtracts 10 from the timer
     if (questions[questionNumber].a === userChoice.textContent){
         document.getElementById('result').textContent = "Correct";
-        console.log(document.getElementById('result'));
+         console.log(document.getElementById('result'));
         highScoreEl = highScoreEl + 15;
-        console.log(highScoreEl);
+         console.log(highScoreEl);
     }
     else { 
         document.getElementById('result').textContent = "Incorrect";
-        console.log(document.getElementById('result'));
-        console.log(highScoreEl);
-}
-        console.log(userChoice.textContent)
- //   var questionNumber = questions[]
-nextQuestion(questionNumber++);
-
+        timerMax -=10;
+          console.log(document.getElementById('result'));
+          console.log(highScoreEl);
+    }
+    //checks to see if there are any more questions to display
+    questionCheck();
 };
+//checks to see if user is on the last question, if so moves to the end page otherwise goes to the next question
+function questionCheck(){
+    if (questionNumber === (questions.length-1)){
+        endQuiz();
+    } else {
+        nextQuestion(questionNumber++);
+    }
+};
+
+// ends the quiz, displays user score
+function endQuiz() {
+    endSection.classList.remove('hide');
+    quizBox.classList.add('hide');
+    document.getElementById('score').textContent= "Your score is:  " + highScoreEl;
+    clearInterval(StartTime);
+}
 
 startButton.addEventListener('click', startQuiz);
